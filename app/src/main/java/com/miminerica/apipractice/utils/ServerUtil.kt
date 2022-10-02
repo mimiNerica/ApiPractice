5c8dac82924ebdb4fbe3e60b6a318196e2c0b8fe
mimi <kuni2097@gmail.com>
2022-10-02 22:01:23 +0900

    로그인 요청 추가

--- app/src/main/java/com/miminerica/apipractice/utils/ServerUtil.kt
+++ app/src/main/java/com/miminerica/apipractice/utils/ServerUtil.kt
@@ -1,12 +1,28 @@
 package com.miminerica.apipractice.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil {
 
     companion object {
 
         val BASE_URL = "http://54.180.52.26"
 
-        fun postRequestLogin() {
+        fun postRequestLogin(email : String, pw : String) {
+
+            val urlString = "${BASE_URL}/user"
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .build()
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+            val client = OkHttpClient()
+            client.newCall(request)
 
         }
     }

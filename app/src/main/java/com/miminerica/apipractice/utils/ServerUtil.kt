80129d585dd1a284a3dc91eaa3de8945e3fe3dfa
mimi <kuni2097@gmail.com>
2022-10-02 21:45:35 +0900

    로그인 화면 작성

---
+++ app/src/main/java/com/miminerica/apipractice/utils/ServerUtil.kt
@@ -0,0 +1,13 @@
+package com.miminerica.apipractice.utils
+
+class ServerUtil {
+
+    companion object {
+
+        val BASE_URL = "http://54.180.52.26"
+
+        fun postRequestLogin() {
+
+        }
+    }
+}
